21b117a8c7e5180c33c44eaaaedceb4cb1d3fa33
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2023-05-31 22:33:56 +0530

    Project

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 
@@ -6,7 +7,25 @@ def index(request):
 	return render(request,'login.html')
 
 def register(request):
-	return render(request,'register.html')
+	if request.method=="POST":
+		try:
+			user=User.objects.get(email=request.POST['email'])
+			msg="Email already registered"
+			return render(request,'register.html',{'msg':msg})
+		except:
+			if request.POST['password'] == request.POST['cpassword']:
+				user=User.objects.create(
+						name=request.POST['name'],
+						email=request.POST['email'],
+						password=request.POST['password']
+					)
+				msg="User registered successfully"
+				return render(request,'login.html',{'msg':msg})
+			else:
+				msg="Password and Confirm Password does not match"
+				return render(request,'register.html',{'msg':msg})
+	else:
+		return render(request,'register.html')
 
 def home(request):
 	return render(request,'sidebar.html')
